54925612b20df1272b2348206515981747c3bd42
yashVSawant <yashsawant23910@gmail.com>
2023-12-24 17:45:39 +0530

    made app real time

--- controllers/message.js
+++ controllers/message.js
@@ -3,11 +3,21 @@ const user = require('../models/user');
 
 exports.getMessages = async(req,res,next)=>{
     try{
-        const chats = await message.findAll({
-            attributes:['text','UserEmail'],
-            include: [{ model: user, attributes: ['name'] }]
-        })
-        res.status(200).json({chats});
+        const count = req.query.count;
+        // console.log('count>>>',count)
+        const totalMsg = await message.count();
+        // console.log(totalMsg)
+        if(count!=totalMsg){
+            const chats = await message.findAll({
+                offset:+count,
+                attributes:['text','UserEmail'],
+                include: [{ model: user, attributes: ['name'] }]
+            })
+            res.status(200).json({success:true,chats,totalMsg});
+        }else{
+            res.status(200).json({success:false});
+        }
+        
     }catch(err){
         console.log(err)
         res.status(404).json({success:false,err})
--- frontend/chatHomePage/chatHomePage.js
+++ frontend/chatHomePage/chatHomePage.js
@@ -3,17 +3,30 @@ const token = localStorage.getItem('grpChatappToken');
 const send = document.getElementById('send');
 
 window.addEventListener('DOMContentLoaded',async()=>{
-    const chats = await axios.get('http://localhost:3000/message/getMessages',{headers:{'Authorization':token}});
-    chats.data.chats.forEach((item)=>{
-        printMessage(item.text ,item.User.name)
-        // console.log(item)
-    })   
+    // const chats = await axios.get('http://localhost:3000/message/getMessages',{headers:{'Authorization':token}});
+    // chats.data.chats.forEach((item)=>{
+    //     printMessage(item.text ,item.User.name)
+    //     // console.log(item)
+    // })   
+    let count =1;
+    setInterval(async()=>{
+        
+        const chats = await axios.get(`http://localhost:3000/message/getMessages?count=${count}`,{headers:{'Authorization':token}});
+        if(chats.data.success){
+                chats.data.chats.forEach((item)=>{
+                printMessage(item.text ,item.User.name)
+                // console.log(item)
+                }) 
+                count = chats.data.totalMsg;
+        }
+           
+    },1000)
 })
 
 send.addEventListener('click',async()=>{
     const text = document.getElementById('text').value;
-    const sendMsg = await axios.post('http://localhost:3000/message/postMessage',{text},{headers:{'Authorization':token}});
-    console.log(sendMsg.data);
+    await axios.post('http://localhost:3000/message/postMessage',{text},{headers:{'Authorization':token}});
+    
 })
 
 
