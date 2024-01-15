18ccd399639f6392a762838d988a1448851fbf19
yashVSawant <yashsawant23910@gmail.com>
2024-01-15 13:54:23 +0530

    edited host name

--- frontend/forgotPasssword/forgotPasssword.js
+++ frontend/forgotPasssword/forgotPasssword.js
@@ -1,10 +1,9 @@
 const submit = document.getElementById('submit');
-const host ='http://localhost:3000'
 const token = localStorage.getItem('grpChatappToken');
 
 submit.addEventListener('click',async(e)=>{
     e.preventDefault()
     console.log(token)
     const email = document.getElementById('email').value;
-    await axios.post(`${host}/user/getResetPasswordMail`,{email},{headers:{'Authorization':token}})
+    await axios.post(`/user/getResetPasswordMail`,{email},{headers:{'Authorization':token}})
 })
--- frontend/signup/signup.js
+++ frontend/signup/signup.js
@@ -7,7 +7,6 @@ signup.addEventListener('click',async(e)=>{
             const email = document.getElementById('email').value;
             const phoneNo = document.getElementById('phone').value;
             const password = document.getElementById('password').value;
-            // console.log(name ,email, phoneNo, password)
             const postUserInfo = await axios.post('/user/signup',{name ,email, phoneNo, password});
             alert(postUserInfo.data.message);
             location.href='../login/index.html';
